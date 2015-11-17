25952a363d2c8d0011a637a16bd480d972bc1a89
Frank Bergmann <frank.bergmann@project-open.com>
2015-11-17 18:07:18 +0000

    - replace ad_parameter by im_parameter for OpenACS

--- www/import-2.tcl
+++ www/import-2.tcl
@@ -28,7 +28,7 @@ set admin_p [im_is_user_site_wide_or_intranet_admin $current_user_id]
 
 # Get the file from the user.
 # number_of_bytes is the upper-limit
-set max_n_bytes [ad_parameter -package_id [im_package_filestorage_id] MaxNumberOfBytes "" 0]
+set max_n_bytes [im_parameter -package_id [im_package_filestorage_id] MaxNumberOfBytes "" 0]
 set tmp_filename [ns_queryget upload_file.tmpfile]
 im_security_alert_check_tmpnam -location "import-2.tcl" -value $tmp_filename
 if { $max_n_bytes && ([file size $tmp_filename] > $max_n_bytes) } {
